561910b9f637ae7aef4a32454e13f1a1a5c30e09
Abraham Paul <87931552+abrahampaul07@users.noreply.github.com>
2024-11-01 16:13:59 +0530

    coming soon

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>
-      Home
+    <div className='flex justify-center text-4xl text-yellow-300 bg-gray-900 p-4 permanent-marker-regular'>
+      Coming Soon!!!!
     </div>
   )
 }
